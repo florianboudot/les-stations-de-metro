ebcc429a6785c9517b6d219bee80bf6ec4e23630
kazes <florian.boudot@gmail.com>
2016-06-24 17:34:32 +0200

    clean code a bit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # les-stations-de-metro
 Jeu où il faut placer les stations de métro Parisien sur la carte
+
+# sources
+* données stations de métro Parisiennes :
+
--- page.php
+++ page.php
@@ -2,10 +2,12 @@
 <html>
 <head>
     <meta charset='utf-8'/>
-    <title></title>
+    <title>Les stations de métro</title>
     <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no'/>
+
     <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.20.0/mapbox-gl.js'></script>
     <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.20.0/mapbox-gl.css' rel='stylesheet'/>
+
     <style>
         body {
             margin: 0;
@@ -22,53 +24,59 @@
 </head>
 <body>
 
+<!-- MAP -->
 <div id='map'></div>
 
-<!-- jquery -->
+<!-- JQUERY -->
 <script src="js/jquery-2.2.4.min.js"></script>
 
+<!-- SCRIPT -->
 <script>
     $(window).on('load', function () {
 
         mapboxgl.accessToken = 'pk.eyJ1Ijoia2F6ZXMiLCJhIjoiMjBiMDc0M2UzYTdkY2NjZDZjZDVhZDdjYWMxMWU4NGMifQ.UbQyYB-QiEQklqy7AXI4XA';
         var map = new mapboxgl.Map({
             container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v9',
+            style: 'mapbox://styles/kazes/cippyzs87004qdmm8k67m0nuu',
             center: [2.354216632304883, 48.86083080287793],
             zoom: 12
         });
 
 
-        // load json
-        $.ajax({
-            url: 'metro.geojson',
-            dataType: "json"
-        }).done(function (data) {
-            console.log('data', data);
-            map.addSource("markers", {
-                "type": "geojson",
-                "data": data
-            });
+        map.style.on('load', function(){
+            // load json
+            $.ajax({
+                url: 'metro.geojson',
+                dataType: "json"
+            })
+            .done(function (data) {
+                console.log('data', data);
 
-            map.addLayer({
-                "id": "markers",
-                "type": "symbol",
-                "source": "markers",
-                "layout": {
-                    "text-field": "{label}",
-                    "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-                    "text-offset": [0, 0.6],
-                    "text-anchor": "top"
-                }
+
+                map.addSource("markers", {
+                    "type": "geojson",
+                    "data": data
+                });
+
+                map.addLayer({
+                    "id": "markers",
+                    "type": "symbol",
+                    "source": "markers",
+                    "paint": {
+                        "text-color": "#ffffff"
+                    },
+                    "layout": {
+                        "text-field": "{label}",
+                        "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+                        "text-offset": [0, 0.6],
+                        "text-anchor": "top"
+                    }
+                });
             });
 
             window._map = map;
         });
-
-
     });
-
-
 </script>
 </body>
 </html>
